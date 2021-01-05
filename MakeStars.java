0c1e8bfa0304ea4064f464b11f1da57c6f5ede8e
Ken Li <kli22@stuy.edu>
2021-01-05 18:21:28 -0500

    Converted everything to stars; not sure if we need to convert punctuation?

--- MakeStars.java
+++ MakeStars.java
@@ -3,8 +3,27 @@ public class MakeStars{
   public static void main( String[]args ){
       Scanner n = new Scanner( System.in );
       while (n.hasNextLine()){
+        Scanner CurrentLine = new Scanner(n.nextLine());
+        while (CurrentLine.hasNext()){
+          String a = CurrentLine.next();
+          for (int i = 0; i < a.length(); i++){
+            System.out.print("*");
+          }
+          System.out.print(" ");
+        }
+      }
+      System.out.println('\n');
+  }
+}
+
+/*
+public class MakeStars2{
+  public static void main( String[]args ){
+      Scanner n = new Scanner( System.in );
+      while (n.hasNextLine()){
         String nextLine = n.nextLine();
         System.out.println(nextLine);
       }
   }
 }
+*/
