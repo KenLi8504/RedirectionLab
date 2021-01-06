2ea7cfad8e7e99116d9ba282e3a200d17807b963
Ken Li <kli22@stuy.edu>
2021-01-05 22:29:35 -0500

    actually completed part 2, not just a test

--- PigLatin.java
+++ PigLatin.java
@@ -20,10 +20,10 @@ public class PigLatin{
     String FirstTwo = s.substring(0,2);
     for (int i = 0; i < Diagraphs.length; i++){
       if (FirstTwo.equals(Diagraphs[i]) ) {
-        return "a";
+        return s.substring(2,s.length())+s.substring(0,2)+"ay";
       }
     }
-    return "b";
+    return (pigLatinSimple(s));
   }
 
   public static void main (String args []){
@@ -31,9 +31,10 @@ public class PigLatin{
     System.out.println(pigLatinSimple("pie"));
     System.out.println(pigLatinSimple("david"));
     System.out.println(pigLatinSimple("aaron"));
-    System.out.println(pigLatin("blewwe"));
-    System.out.println(pigLatin("sdklvjsd"));
-    System.out.println(pigLatin("drsadfafd"));
-    System.out.println(pigLatin("whasdfas"));
+    System.out.println(pigLatin("the"));
+    System.out.println(pigLatin("check"));
+    System.out.println(pigLatin("skee"));
+    System.out.println(pigLatin("emu"));
+    System.out.println(pigLatin("grade"));
   }
 }
